76a9007968d589508a30ce633aa9625b10ac2316
cutnasywaymn <80031100+cutnasywaymn@users.noreply.github.com>
2021-04-03 16:43:36 +0700

    menambahkan fitur edit dan hapus

--- home.php
+++ home.php
@@ -61,8 +61,10 @@
 	 			<th>Program Studi</th>
 	 			<th>edit/hapus</th>
 	 		</tr>
-	 				<a href="home.php" class="btn btn-warning">Edit</a>
-	 				<a href="home.php" onclick="return confirm('apakah ingin menghapus data?')" class="btn btn-danger">Hapus</a>
+	 			<td>
+          <a href="#" class="btn btn-warning">Edit</a>
+          <a href="#" onclick="return confirm('apakah ingin menghapus data?')" class="btn btn-danger">Hapus</a>
+        </td>
 	 			</td>
 	 		</tr>
  		</table>
